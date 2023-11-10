399a9c9ca4b97a2a4f5d5a76a0092512053b3bd1
vakiyama <vakiyama@outlook.com>
2023-11-09 17:43:39 -0800

    cleanup comments

--- IOHandler.ts
+++ IOHandler.ts
@@ -1,9 +1,3 @@
-/*
- * Project: Milestone 1
- * File Name: IOhandler.ts
- * Description: Collection of functions for files input/output related operations
- */
-
 import AdmZip from "adm-zip";
 import {
     readdir,
@@ -18,28 +12,18 @@ import path from "path";
 
 export type Filter = (this: PNG) => Buffer;
 
-/**
- * Description: decompress file from given pathIn, write to given pathOut
- */
 export function unzip(pathIn: string, pathOut: string): void {
     const zip = new AdmZip(pathIn);
     zip.extractAllTo(pathOut, true);
     console.log("Extraction operation complete");
 }
 
-/**
- * Description: read all the png files from given directory and return Promise containing array of each png file path
- */
 export async function readDir(dir: string): Promise<string[]> {
     const dirs = await readdir(dir);
     const paths = dirs.filter((file) => path.extname(file) === ".png");
     return paths.map((file) => path.join(dir, file));
 }
 
-/**
- * Description: Read in png file by given pathIn,
- * convert to grayscale and write to given pathOut
- */
 export async function filterPNG(
     pathIn: string,
     pathOut: string,
