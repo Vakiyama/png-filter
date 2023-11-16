9cdbebd756d10bd7c1b4f7c35e90aa0e668ad7ee
vakiyama <vakiyama@outlook.com>
2023-11-16 13:19:30 -0800

    added inverted filter

--- filters.ts
+++ filters.ts
@@ -54,6 +54,16 @@ export function dithering(data: Buffer): Buffer {
     return data;
 };
 
+export function inverted(data: Buffer): Buffer {
+    for (let i = 0; i < data.length; i += 4) {
+        data[i] = 255 - data[i];
+        data[i + 1] = 255 - data[i + 1];
+        data[i + 2] = 255 - data[i + 2];
+    }
+
+    return data;
+}
+
 function setRGBValues(
     data: Buffer,
     i: number,
